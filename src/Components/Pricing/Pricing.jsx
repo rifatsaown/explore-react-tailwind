4bcbb7a9eebae16bcd49fe0b3fc721cf32032062
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-02-05 16:59:46 +0600

    card added

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
-import PricingOption from '../PricingOption/PricingOption';
+import React from "react";
+import PricingOption from "../PricingOption/PricingOption";
 
 const Pricing = () => {
-    const pricingList = [
-        {id : 1, name : 'Basic', price : 100,},
-        {id : 2, name : 'Standard', price : 200,},
-        {id : 3, name : 'Premium', price : 300,},
-    ];
-    return (
-        <div>
-            <h1 className='text-5xl font-semibold bg-purple-400 p-4 pb-5'>Best Price in city</h1>
-            {
-                pricingList.map((option) => (<PricingOption key={option.id} option={option} />) )
-            }
-        </div>
-    );
+  const pricingList = [
+    {
+      id: 1,
+      name: "Basic",
+      price: 100,
+      fetures: [
+        "1GB Storage",
+        "10GB Bandwidth",
+        "5 Email Accounts",
+        "5 Subdomains",
+        "5 Domains",
+      ],
+    },
+    {
+      id: 2,
+      name: "Standard",
+      price: 200,
+      fetures: [
+        "2GB Storage",
+        "20GB Bandwidth",
+        "10 Email Accounts",
+        "10 Subdomains",
+        "10 Domains",
+      ],
+    },
+    {
+      id: 3,
+      name: "Premium",
+      price: 300,
+      fetures: [
+        "3GB Storage",
+        "30GB Bandwidth",
+        "15 Email Accounts",
+        "15 Subdomains",
+        "15 Domains",
+      ],
+    },
+  ];
+  return (
+    <div>
+      <h1 className="text-5xl font-semibold bg-purple-400 p-4 pb-5">
+        Best Price in city
+      </h1>
+      <div className="grid grid-cols-3 gap-4 m-10">
+        {pricingList.map((option) => (
+          <PricingOption key={option.id} option={option} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Pricing;
--- src/Components/PricingOption/PricingOption.jsx
+++ src/Components/PricingOption/PricingOption.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React from "react";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 
-const PricingOption = ({option}) => {
-    return (
-        <div>
-            <div>
-                <p>{option.price}</p>
-                <h3>{option.name}</h3>
-            </div>
+const PricingOption = ({ option }) => {
+  return (
+    <div className=" bg-purple-400 p-10 rounded-xl mx-10">
+      <div>
+        <h2 className=" text-slate-100">
+          <span className="text-5xl font-bold text-white">{option.price}</span>
+          /m
+        </h2>
+        <p className="text-3xl my-3 font-semibold">{option.name}</p>
+      </div>
+      {option.fetures.map((feture, index) => (
+        <div key={index} className="flex items-center ml-3 mt-6">
+          <IoIosCheckmarkCircleOutline className="w-6 h-6" />
+          <p className="text-xl font-semibold ml-2">{feture}</p>
         </div>
-    );
+      ))}
+      <button className="bg-green-500 font-semibold text-white rounded-md px-4 py-2 md:mb-0 mt-4 hover:bg-green-400">
+        Buy Now
+      </button>
+    </div>
+  );
 };
 
 export default PricingOption;
